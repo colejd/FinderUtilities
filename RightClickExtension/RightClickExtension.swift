3586d44eeb88fdba4140c5220320e7e15095833d
Jonathan Cole <joncole04412@gmail.com>
2020-11-06 11:04:00 -0500

    Modify right-click options, add open in editor option, icons

--- RightClickExtension/RightClickExtension.swift
+++ RightClickExtension/RightClickExtension.swift
@@ -8,6 +8,37 @@
 
 import Cocoa
 import FinderSync
+import ProcessRunner
+
+struct AppInfo {
+    let url: URL
+
+    init?(forAppWithPath path: String) {
+        guard path.hasSuffix(".app") else {
+            NSLog("The path \"\(path)\" must end in \".app\"!")
+            return nil
+        }
+        guard FileManager.default.fileExists(atPath: path) else {
+            NSLog("The app specified with the path \"\(path)\" does not exist")
+            return nil
+        }
+        url = URL(fileURLWithPath: path)
+    }
+
+    var name: String {
+        return url.deletingPathExtension().lastPathComponent
+    }
+
+    var icon: NSImage {
+        return NSWorkspace.shared.icon(forFile: url.path)
+    }
+}
+
+struct Apps {
+    static let terminal = AppInfo(forAppWithPath: "/System/Applications/Utilities/Terminal.app")!
+    static let textEdit = AppInfo(forAppWithPath: "/System/Applications/TextEdit.app")!
+    static let visualStudioCode = AppInfo(forAppWithPath: "/Applications/Visual Studio Code.app")!
+}
 
 class FinderSync: FIFinderSync {
     
@@ -24,8 +55,11 @@ class FinderSync: FIFinderSync {
 
         // Produce a menu for the extension (to be shown when right clicking a folder in Finder)
         let menu = NSMenu(title: "")
-        menu.addItem(withTitle: "Open Terminal", action: #selector(openTerminalClicked(_:)), keyEquivalent: "")
-        menu.addItem(withTitle: "Create empty.txt", action: #selector(createEmptyFileClicked(_:)), keyEquivalent: "")
+        let terminalItem = menu.addItem(withTitle: "Open in Terminal", action: #selector(openTerminalClicked(_:)), keyEquivalent: "")
+        terminalItem.image = Apps.terminal.icon
+        let editorItem = menu.addItem(withTitle: "Open in Editor", action: #selector(openInEditorClicked(_:)), keyEquivalent: "")
+        editorItem.image = Apps.visualStudioCode.icon
+        menu.addItem(withTitle: "Create empty file here", action: #selector(createEmptyFileClicked(_:)), keyEquivalent: "")
         menu.addItem(withTitle: "Copy selected paths", action: #selector(copyPathToClipboard), keyEquivalent: "")
 
         return menu
@@ -35,10 +69,7 @@ class FinderSync: FIFinderSync {
     @IBAction func copyPathToClipboard(_ sender: AnyObject?) {
         
         guard let target = FIFinderSyncController.default().selectedItemURLs() else {
-            
-            
             NSLog("Failed to obtain targeted URLs: %@")
-
             return
         }
         
@@ -58,26 +89,11 @@ class FinderSync: FIFinderSync {
 
     /// Opens a macOS Terminal.app window in the user-chosen folder
     @IBAction func openTerminalClicked(_ sender: AnyObject?) {
-        
-        guard let target = FIFinderSyncController.default().targetedURL() else {
-            
-            NSLog("Failed to obtain targeted URL: %@")
-            
-            return
-        }
-        
-        let task = Process()
-        task.executableURL = URL(fileURLWithPath: "/usr/bin/open")
-        task.arguments = ["-a", "terminal", "\(target)"]
-        
-        do {
-            
-            try task.run()
+        openApp(withAppInfo: Apps.terminal)
+    }
 
-        } catch let error as NSError {
-            
-            NSLog("Failed to open Terminal.app: %@", error.description as NSString)
-        }
+    @IBAction func openInEditorClicked(_ sender: AnyObject?) {
+        openApp(withAppInfo: Apps.visualStudioCode)
     }
 
     /// Creates an empty file with name "untitled" under the user-chosen Finder folder.
@@ -92,13 +108,13 @@ class FinderSync: FIFinderSync {
         }
 
         var originalPath = target
-        let originalFilename = "newfile"
-        var filename = "newfile.txt"
-        let fileType = ".txt"
+        let originalFilename = "untitled"
+        let fileType = ""
+
         var counter = 1
-        
+        var filename = "untitled"
         while FileManager.default.fileExists(atPath: originalPath.appendingPathComponent(filename).path) {
-            filename = "\(originalFilename)\(counter)\(fileType)"
+            filename = "\(originalFilename) \(counter)\(fileType)"
             counter+=1
             originalPath = target
         }
@@ -106,8 +122,35 @@ class FinderSync: FIFinderSync {
         do {
             try "".write(to: target.appendingPathComponent(filename), atomically: true, encoding: String.Encoding.utf8)
         } catch let error as NSError {
-            
             NSLog("Failed to create file: %@", error.description as NSString)
         }
+    }
+
+    func openApp(withAppInfo info: AppInfo) {
+        guard let target = FIFinderSyncController.default().targetedURL() else {
+            NSLog("Failed to obtain targeted URL: %@")
+            return
+        }
+
+//        let task = Process()
+//        task.executableURL = URL(fileURLWithPath: "/usr/bin/open")
+//        task.currentDirectoryURL = target
+//        task.arguments = ["-a", info.name, "\(target.absoluteString)"]
+//
+//        do {
+//            try task.run()
+//            task.waitUntilExit()
+//        } catch let error as NSError {
+//            NSLog("Failed to open \"\(info.name)\": %@", error.description as NSString)
+//        }
+        let f = target.startAccessingSecurityScopedResource()
+        let command = "open -a \(info.name) \(target.path)"
+        let currentDirectory = target.isFileURL ? target.deletingLastPathComponent() : target
+        let result = try! system(command: command, captureOutput: true)
+        currentDirectory.stopAccessingSecurityScopedResource()
+        NSLog(result.standardOutput)
+        NSLog(result.standardError)
+        NSLog("\(result.success)")
+        let x = 5
     }
 }
